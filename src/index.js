3ea77685dfd181ecc709a5dbb427d5532a9f2245
nikhil11001 <nikhil11001@gmail.com>
2018-06-04 21:06:29 -0700

    Added routes in index.js to render routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import AppRoutes from "./routes";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<AppRoutes/>, document.getElementById('root'));
 registerServiceWorker();
